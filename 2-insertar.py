dec4db8e4cc24edaf78223de90fd6135d20ab793
gustavodp22 <90285790+gustavodp22@users.noreply.github.com>
2021-10-02 20:55:38 -0300

    Add files via upload

---
+++ 2-insertar.py
@@ -0,0 +1,18 @@
+"""
+CRUD de SQLite3 con Python 3
+
+"""
+import sqlite3
+try:
+	bd = sqlite3.connect("libros.db")
+	cursor = bd.cursor()
+	autor = input("\nAutor: ")
+	genero = input("\nGénero: ")
+	precio = float(input("\nPrecio: "))
+	titulo = input("\nTítulo: ")
+	sentencia = "INSERT INTO libros(autor, genero, precio, titulo) VALUES (?,?,?,?)"
+	cursor.execute(sentencia, [autor, genero, precio, titulo])
+	bd.commit()
+	print("Guardado correctamente")
+except sqlite3.OperationalError as error:
+	print("Error al abrir:", error)
