6ba291103129225b31044e373b077a4f083398fa
gustavodp22 <90285790+gustavodp22@users.noreply.github.com>
2021-10-02 20:56:22 -0300

    Add files via upload

---
+++ 3-leer.py
@@ -0,0 +1,17 @@
+"""
+CRUD de SQLite3 con Python 3
+
+"""
+import sqlite3
+try:
+	bd = sqlite3.connect("libros.db")
+	cursor = bd.cursor()
+	sentencia = "SELECT * FROM libros;"
+ 
+	cursor.execute(sentencia)
+	
+	libros = cursor.fetchall()
+	
+	print(libros)
+except sqlite3.OperationalError as error:
+	print("Error al abrir:", error)
